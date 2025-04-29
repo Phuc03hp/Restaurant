0a09999328ac24506ed151b38bd0ca6f73009bac
TungYeah <minhtung2108@gmail.com>
2025-04-29 11:14:45 +0700

    fix

--- src/main/java/phuc.devops.tech.restaurant/Controller/UserController.java
+++ src/main/java/phuc.devops.tech.restaurant/Controller/UserController.java
@@ -1,5 +1,6 @@
 package phuc.devops.tech.restaurant.Controller;
 
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import phuc.devops.tech.restaurant.Entity.User;
@@ -8,7 +9,7 @@ import phuc.devops.tech.restaurant.dto.request.AdminCreateAccountUser;
 import phuc.devops.tech.restaurant.dto.request.AdminUpdateAccountUser;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/auth")
 public class UserController {
@@ -23,14 +24,14 @@ public class UserController {
         return userService.getUsers();
     }
     @PutMapping("/signup/user/{userID}")
-    User updateUser(@PathVariable String userID, AdminUpdateAccountUser request){
-        return userService.updateUser(userID,request);
+    public User updateUser(@PathVariable String userID, @RequestBody AdminUpdateAccountUser request) {
+        return userService.updateUser(userID, request);
     }
     @GetMapping("/signup/user/{userID}")
     User getUser(@PathVariable String userID){
         return userService.getUser(userID);
     }
-    @DeleteMapping("/signup/user/{userID}")
+    @DeleteMapping("/users/{userID}")
     String deleteUser(@PathVariable String userID){
         userService.deleteUser(userID);
         return "User has been deleted";
@@ -39,9 +40,9 @@ public class UserController {
     User findByUsername(@PathVariable String username){
         return userService.findByUsername(username);
     }
-
     @GetMapping("/users")
-    List<User> getListUser(){
+    public List<User> getUser() {
         return userService.getUsers();
     }
+
 }
